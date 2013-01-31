d9e7f3678168a76045ec2e4f2ae897b67de59b16
Michael Phillips <phillimj@mail.gvsu.edu>
2013-01-31 17:34:58 -0500

    Add feature to convert from image format into PPM, stored in a Blob. Read/write data from Blob into stringstream. Read/write prefix data from stringstream into memory, then output data.

--- convert.cpp
+++ convert.cpp
@@ -1,5 +1,8 @@
+#include <iostream>
+#include <sstream>
 #include <Magick++.h>
 
+using namespace std;
 using namespace Magick;
 
 int main (int argc, char *argv[])
@@ -9,8 +12,30 @@ int main (int argc, char *argv[])
 	Image image;
 
 	image.read(argv[1]);
+
+	// Setting quality to 0 ensures that the formatting is not compressed to binary.
+	// Instead, the file is converted to an ASCII formatt.
+	// See PNM section: http://www.graphicsmagick.org/formats.html
+	// Also, see: http://www.graphicsmagick.org/formats.html#quality
 	image.quality(0);
-	image.write(argv[2]);
+	image.magick( "PPM" );
+
+	Blob blob;
+	image.write( &blob );
+
+	stringstream ppmStream;
+
+	ppmStream << (char*) blob.data();
+
+	char pre[2];
+	int c, r, scale;
+	ppmStream >> pre;
+	ppmStream >> c;
+	ppmStream >> r;
+	ppmStream >> scale;
+	
+	cout << pre[0] << pre[1] << endl;
+	cout << c << ", " << r << ", " << scale << endl;
 
 	return 0;
 }
